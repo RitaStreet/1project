814a3221f941542ed9e9b60897fe63f47e35f5d1
RitaStreet <62694595+RitaStreet@users.noreply.github.com>
2020-06-09 04:46:23 +0600

    Update main.c

--- main.c
+++ main.c
@@ -5,68 +5,77 @@
 #define OS_TYPE linux
 #define maxlen 256
 
-typedef struct numb
+typedef struct stroka
 {
-    int num;
-    struct numb *next;
-}numb;
+    char *str;
+    struct stroka *next;
+}stroka;
 
-void push(numb **head, int num)//функция добавляющая элемент стека
+stroka *push(stroka *head, char *s1, int slen)//функция добавляющая элемент стека
 {
-    numb *p = (numb*)malloc(sizeof(numb));
+    stroka *p = NULL;
+    p = (stroka*)malloc(sizeof(stroka));
+    p -> str = (char*)malloc((slen+1) * sizeof(char));
     if (p != NULL)
     {
-        p -> next = *head;
-        p -> num = num;
-        *head = p;//элемент становится головой, тем самым занимая первое место и сдвигая предыдущую голову вперёд на шаг
+        p -> next = head;
+        strcpy(p -> str, s1);
+        head = p;//элемент становится головой, тем самым занимая первое место и сдвигая предыдущую голову вперёд на шаг
     }
     else
         puts("Error!");
+    return head;
 }
 
-void pop(numb **head)//функция "удаляющая" элемент стека
+stroka *pop(stroka *head)//функция "удаляющая" элемент стека
 {
-    if ((*head) != NULL)
-        *head = (*head) -> next;//двигаем голову списка вперёд на 1
+    if (head != NULL)
+        head = head -> next;//двигаем голову списка вперёд на 1
     else
         puts("Error!");
+
+    return head;
 }
 
 int main()
 {
-    numb *head = NULL;
+    stroka *head = NULL;
     FILE *df, *mf;
-    int k, zn;
-    float aver;
-    aver = 0;
-    k = 0;
+    int sumstr, n, i, slen, *a = NULL;
+    char s1[maxlen];
+    sumstr = 0;
 
-    df = fopen("stek.txt", "r");//считываем файл из чисел которого формируется стек
+    df = fopen("csv.txt", "r");//считываем файл из чисел которого формируется стек
     mf = fopen("res.txt", "w");//создаём файл в который будут записаны нечётные числа
     if (df != NULL)
     {
-        zn = 0;
-        while(fscanf(df, "%d", &zn) != EOF)//считываем каждое число
+        n = 0;
+        while ((fgets(s1, maxlen, df)) != NULL) n++;
+        rewind(df);
+        a = malloc(n * sizeof(int));
+
+        for (i = 0; i < n; i++)
         {
-            printf("%d\n", zn);
-            push(&head, zn);//добавляем элемент в стек
-            if (zn % 2 == 1)
-            {
-                k++;
-                aver += zn;
-                fprintf(mf, "%d\n", zn);//проверяем на нечётность, и если это так то записываем число в файл
-            }
+            fgets(s1, maxlen, df);
+            slen = strlen(s1);
+            s1[slen - 1] = '\0';
+            slen = strlen(s1);
+            a[i] = slen;
+            sumstr += slen;
+            head = push(head, s1, slen);//добавляем элемент в стек
+            printf("%s\n", head -> str);
 
         }
-        pop(&head);//удаляем элемент стека(для проверки работы функции)
+        head = pop(head);
+        head = pop(head);//удаляем 2 элемента по заданию
+        while(head != NULL)
+        {
+            fprintf(mf, "%s\n", head -> str);
+            head = pop(head);
+        }
     }
+    sumstr = sumstr - a[n - 1] - a[n - 2];
+    printf("Dlina strok tekushego steka = %d", sumstr);
     rewind(df);//переводим указатель для красоты
-    if(aver)
-    {
-        aver = aver/k;
-        printf("\nOdd average - %f", aver);
-    }
-    else
-        puts("No odd elements!");
     return 0;
 }
